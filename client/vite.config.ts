7fe040c2007ca8ca4a108b60fe8984cff934e81c
Ian <ian@student.dev-island.org>
2024-06-11 21:32:42 -1000

    vite config for env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {...process.env,
+      VITE_ROOT: process.env.VITE_ROOT}
+  }
   
 })
