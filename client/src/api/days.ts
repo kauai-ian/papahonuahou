cd60502e172230d3498a0264e4ba76e132e1c5fa
Ian <ian@student.dev-island.org>
2024-06-11 21:19:15 -1000

    moved env to root

--- client/src/api/days.ts
+++ client/src/api/days.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const ROOT = import.meta.env.VITE_ROOT;
+console.log("API Root URL:", ROOT);
 
 export const listDays = async () => {
   const response = await axios.get(`${ROOT}/days`);
--- client/src/pages/Statistics.tsx
+++ client/src/pages/Statistics.tsx
@@ -2,16 +2,21 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
 import StatisticsForm from '../components/StatsForm';
-import { ToggleStatisticsComponent } from '../components/ToggleStatistics';
+// import { ToggleStatisticsComponent } from '../components/ToggleStatistics';
 
 
 // TODO Render the statistics toggle buttons
 
 const StatisticsPage: React.FC = () => {
+
   return (
     <Box p={4}>
       <h1>Statistics</h1>
-      <ToggleStatisticsComponent/>
+      {/* <ToggleStatisticsComponent filter={{
+        eventType: '',
+        eventStart: undefined,
+        eventEnd: undefined
+      }}  /> */}
 
       <StatisticsForm />
     </Box>
